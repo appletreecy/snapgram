e9120cda2d4185bcc24d85d4bbd1bff336770717
Steven Cao <steven.cao@bytedance.com>
2024-04-27 22:04:53 +1000

    before deployment

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -5,12 +5,12 @@ import { Loader } from "lucide-react";
 import React, { useState, useEffect } from "react";
 
 type PostStatsProps = {
-    post: Models.Document;
+    post?: Models.Document;
     userId: string;
 }
 
 const PostStats = ({ post, userId }: PostStatsProps) => {
-    const likesList = post.likes.map((user: Models.Document) => user.$id)
+    const likesList = post?.likes.map((user: Models.Document) => user.$id)
 
     const [likes, setLikes] = useState(likesList);
     const [isSaved, setIsSaved] = useState(false);
@@ -23,7 +23,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
 
 
     const savedPostRecord = currentUser?.save.find(
-        (record: Models.Document) => record.post.$id === post.$id
+        (record: Models.Document) => record.post.$id === post?.$id
     );
 
     useEffect(() => {
@@ -44,7 +44,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
         }
 
         setLikes(newLikes)
-        likePost({ postId: post.$id, likesArray: newLikes })
+        likePost({ postId: post?.$id || "", likesArray: newLikes })
 
     }
 
@@ -57,7 +57,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
             setIsSaved(false);
             deleteSavedPost(savedPostRecord.$id);
         } else {
-            savePost({ userId: userId, postId: post.$id });
+            savePost({ userId: userId, postId: post?.$id || "" });
             setIsSaved(true);
         }
     }
